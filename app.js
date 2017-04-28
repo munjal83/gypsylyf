72d9e7e4f6783f3771e343cf31a5f8b6dfc7bdd9
munjal83 <munjalbpatel@hotmail.com>
2017-04-28 10:09:13 +0000

    Add env variable for database connection

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var parkRoutes = require("./routes/parks");
 var indexRoutes = require("./routes/index");
 
 //mongoose.connect("mongodb://localhost/gypsylyf");
-mongoose.connect("mongodb://munjal:1234qazx@ds123381.mlab.com:23381/gypsylyf");
+mongoose.connect(process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
